e7f282c0a9aa5bea09bbb57f6b53385b95082dd2
Baala-Murugan-K <baala25harish01@gmail.com>
2025-12-30 21:49:37 +0530

    initial commit

--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
 
-const databaseConnection = () => {
-    mongoose.connect(process.env.DB,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then((data)=>{
-        console.log(`database connected successfully at server ${data.connection.host}`)
-    })
+const databaseConnection = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('Database connected successfully')
+    }
+    catch (error) {
+        console.error('Database connection failed:', error)
+        process.exit(1)
+    }
+
 }
 
 module.exports = databaseConnection
--- server/server.js
+++ server/server.js
@@ -3,18 +3,16 @@ const databaseConnection = require('./config/database')
 const cloudinary  = require('cloudinary')
 const dotenv = require('dotenv')
 dotenv.config({path:"./config/config.env"})
-
-
-cloudinary.config({
+require('dotenv').config()
+cloudinary.config({         
     cloud_name: process.env.CLOUDINARY_NAME ,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 }) 
-
 databaseConnection()
 
 
-
+const PORT = process.env.PORT || 5000
 app.listen(process.env.PORT,()=>{
     console.log(`server is running on port ${process.env.PORT}`)
 })
